ee30a674764ecec96e1c2480840f6d407d188c12
prdpta <pradiptaputramei01@gmail.com>
2026-01-17 17:22:32 +0700

    update Models.kt

--- app/src/main/java/com/example/tugasakhirpam_20230140037/data/model/Models.kt
+++ app/src/main/java/com/example/tugasakhirpam_20230140037/data/model/Models.kt
@@ -2,7 +2,6 @@ package com.example.tugasakhirpam_20230140037.data.model
 
 import com.google.gson.annotations.SerializedName
 
-// 1. Model untuk menangani Login
 data class LoginResponse(
     @SerializedName("status") val status: Boolean,
     @SerializedName("message") val message: String,
@@ -14,7 +13,6 @@ data class UserData(
     @SerializedName("username") val username: String
 )
 
-// 2. Model utama untuk data Catatan & Tugas
 data class CatatanResponse(
     @SerializedName("id_catatan") val id_catatan: Int,
     @SerializedName("id_user") val id_user: Int,
@@ -26,20 +24,17 @@ data class CatatanResponse(
     @SerializedName("created_at") val created_at: String? = null
 )
 
-// 3. Model untuk mengambil data Profil User
 data class ProfileResponse(
     @SerializedName("status") val status: Boolean,
     @SerializedName("id_user") val id_user: Int,
     @SerializedName("username") val username: String
 )
 
-// 4. Model untuk feedback API (Insert, Update, Delete)
 data class ApiResponse(
     @SerializedName("status") val status: Boolean,
     @SerializedName("message") val message: String
 )
 
-// 5. Model untuk data Kategori
 data class KategoriResponse(
     @SerializedName("id_kategori") val id_kategori: Int,
     @SerializedName("nama_kategori") val nama_kategori: String
