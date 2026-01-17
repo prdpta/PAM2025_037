f95f4bcb76f6d30140752d4cc5f33cc98da6d3b8
prdpta <pradiptaputramei01@gmail.com>
2026-01-17 17:21:29 +0700

    update ApiService.kt dan RetrofitClient.kt

--- app/src/main/java/com/example/tugasakhirpam_20230140037/data/api/ApiService.kt
+++ app/src/main/java/com/example/tugasakhirpam_20230140037/data/api/ApiService.kt
@@ -6,7 +6,6 @@ import retrofit2.http.*
 
 interface ApiService {
 
-    // 1. LOGIN
     @FormUrlEncoded
     @POST("login")
     fun login(
@@ -14,25 +13,21 @@ interface ApiService {
         @Field("password") password: String
     ): Call<LoginResponse>
 
-    // 2. PROFILE (PENTING: Menggunakan @Query agar sesuai dengan Node.js get_profile)
     @GET("get_profile")
     fun getProfile(
         @Query("id_user") id_user: Int
     ): Call<ProfileResponse>
 
-    // 3. AMBIL SEMUA CATATAN (Berdasarkan ID User)
     @GET("catatan/{id_user}")
     fun getCatatan(
         @Path("id_user") idUser: Int
     ): Call<List<CatatanResponse>>
 
-    // 4. DETAIL CATATAN (Untuk mengambil data sebelum diedit)
     @GET("catatan/detail/{id}")
     fun getCatatanById(
         @Path("id") id: Int
     ): Call<CatatanResponse>
 
-    // 5. TAMBAH CATATAN (Lengkap dengan id_kategori)
     @FormUrlEncoded
     @POST("catatan")
     fun addCatatan(
@@ -42,7 +37,6 @@ interface ApiService {
         @Field("isi_catatan") isi: String
     ): Call<ApiResponse>
 
-    // 6. UPDATE CATATAN (Menggunakan @Path untuk ID catatan)
     @FormUrlEncoded
     @PUT("catatan/{id}")
     fun updateCatatan(
@@ -51,7 +45,6 @@ interface ApiService {
         @Field("isi_catatan") isi: String
     ): Call<ApiResponse>
 
-    // 7. DELETE CATATAN
     @DELETE("catatan/{id}")
     fun deleteCatatan(
         @Path("id") id: Int
--- app/src/main/java/com/example/tugasakhirpam_20230140037/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/tugasakhirpam_20230140037/data/api/RetrofitClient.kt
@@ -4,11 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    /**
-     * IP 10.0.2.2 adalah alias untuk localhost di Emulator Android.
-     * Sesuaikan PORT (contoh: 3000) dengan port yang digunakan server Node.js kamu.
-     * Jangan lupa garis miring (/) di akhir URL.
-     */
+
     private const val BASE_URL = "http://10.0.2.2:3000/"
 
     val instance: ApiService by lazy {
