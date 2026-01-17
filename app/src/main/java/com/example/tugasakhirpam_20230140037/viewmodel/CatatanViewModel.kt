472028b727c48921db4653bdbc805fab7f5b8cf1
prdpta <pradiptaputramei01@gmail.com>
2026-01-17 17:28:49 +0700

    update CatatanViewModel.kt

--- app/src/main/java/com/example/tugasakhirpam_20230140037/viewmodel/CatatanViewModel.kt
+++ app/src/main/java/com/example/tugasakhirpam_20230140037/viewmodel/CatatanViewModel.kt
@@ -6,7 +6,5 @@ import com.example.tugasakhirpam_20230140037.data.repository.CatatanRepository
 class CatatanViewModel : ViewModel() {
     private val repository = CatatanRepository()
 
-    // Kamu bisa memindahkan logika Retrofit dari Screen ke sini nanti.
-    // Untuk mengejar deadline, biarkan ini sebagai jembatan saja.
     fun getRepository() = repository
 }
